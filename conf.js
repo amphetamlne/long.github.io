b49edcb61f3dd00c1d03f093b9439428d59a5487
acao <55946742+amphetamlne@users.noreply.github.com>
2021-05-07 21:29:29 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '111',
+  ver: '112',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -11,21 +11,15 @@ jsproxy_config({
   },
 
   // 节点配置
-  node_map: {
-    'demo-hk': {
-      label: '香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
-      }
-    },
-    'demo-sg': {
-      label: '新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
+  node_map: {	  
+   //'demo-hk': {
+   //  label: '香港节点',
+   //  lines: {
+   //    // 主机:权重
+   //    'node-aliyun-hk-1.etherdream.com:8443': 1,
+   //    'node-aliyun-hk-2.etherdream.com:8443': 2,
+   //  }
+   //},
     'mysite': {
       label: '当前站点',
       lines: {
@@ -38,14 +32,14 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker-2.etherdream.com': 4,
+        //'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.007.workers.dev': 1,
-        'b.hehe.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
-        'b.jsproxy.workers.dev': 1,
+        'jsproxy1.412503533.workers.dev': 1,
+        'jsproxy2.412503533.workers.dev': 1,
+        'jsproxy3.lc412503533.workers.dev': 1,
+        'jsproxy4.lc412503533.workers.dev': 1,
       }
     }
   },
@@ -53,7 +47,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'demo-hk',
+  node_default: 'mysite',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
@@ -84,5 +78,6 @@ jsproxy_config({
   /**
    * URL 自定义处理（设计中）
    */
-  url_handler: {}
+  url_handler: {
+  }
 })
