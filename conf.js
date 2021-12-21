92966fe3ec49103cf715e262d94ca275ea631d57
acao <55946742+amphetamlne@users.noreply.github.com>
2021-12-21 16:12:59 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,22 +1,17 @@
 jsproxy_config({
-  // 当前配置的版本（记录在日志中，用于排查问题）
-  // 每次修改配置，该值需要增加，否则不会生效。
-  // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '114',
-
+  ver: '118',
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
-    enable: true,
-    ver: 62
+    enable: false,
+    ver: 65
   },
-
   // 节点配置
   node_map: {	  
     'mysite': {
       label: '当前站点',
       lines: {
         'jsproxy.412503533.workers.dev': 1,
-        'js.lc.workers.dev': 1,
+        'jsproxy.lc412503533.workers.dev': 1,
         'jsproxy.acao.workers.dev':1,
         'js.lcsina.workers.dev':1,
         'jsproxy1.412503533.workers.dev': 1,
@@ -31,6 +26,10 @@ jsproxy_config({
         'jsproxy3.lc412503533.workers.dev': 1,
         'jsproxy3.acao.workers.dev':1,
         'js3.lcsina.workers.dev':1,
+        'resource.412503533.workers.dev': 1,
+        'resource.lc412503533.workers.dev': 1,
+        'resource.acao.workers.dev':1,
+        'resource.lcsina.workers.dev':1,
       }
     },
     // 该节点用于加载大体积的静态资源
@@ -39,18 +38,16 @@ jsproxy_config({
       hidden: true,
       lines: {
         'resource.412503533.workers.dev': 1,
-        'res.lc412503533.workers.dev': 1,
+        'resource.lc412503533.workers.dev': 1,
         'resource.acao.workers.dev':1,
         'resource.lcsina.workers.dev':1,
       }
     }
   },
-
   /**
    * 默认节点
    */
-  node_default: 'mysite',
-  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: 'mysite', 
 
   /**
    * 加速节点
@@ -61,21 +58,21 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/amphetamlne/long.github.io@master/assets/',
+  //assets_cdn: 'https://cdn.jsdelivr.net/gh/amphetamlne/long.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  //assets_cdn: 'assets/',
-
+  assets_cdn: 'assets/',
   // 首页路径
   index_path: 'index_v3.html',
 
   // 支持 CORS 的站点列表（实验中...）
   direct_host_list: 'cors_v1.txt',
+
   /**
    * 自定义注入页面的 HTML
    */
   inject_html: '<!-- custom html -->',
-
+  
   /**
    * URL 自定义处理（设计中）
    */
